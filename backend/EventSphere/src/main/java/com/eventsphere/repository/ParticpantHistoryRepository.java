7b819365f8be7c6311c38ab74c434f9e6d8ab0a8
Hyorannunes <hyoran2000@gmail.com>
2025-06-24 22:37:17 -0300

    backend: fix: ParticipantHistory

--- backend/EventSphere/src/main/java/com/eventsphere/repository/ParticpantHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eventsphere.repository;
-
-import com.eventsphere.entity.event.ParticipantHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticpantHistoryRepository extends JpaRepository<ParticipantHistory, Long> {
-}
