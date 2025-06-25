fe468a30653fd8f2da1861e8123ec193f98213b4
Hyorannunes <hyoran2000@gmail.com>
2025-06-24 22:42:09 -0300

    backend: feat: created ParticipantDTO

---
+++ backend/EventSphere/src/main/java/com/eventsphere/dto/ParticipantDTO.java
@@ -0,0 +1,88 @@
+package com.eventsphere.dto;
+
+public class ParticipantDTO {
+    private Long id;
+    private Long userId;
+    private String userName;
+    private String userUsername;
+    private String userEmail;
+    private String userPhoto;
+    private boolean isCollaborator;
+    private String status;
+    private boolean confirmed;
+    
+    public ParticipantDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserUsername() {
+        return userUsername;
+    }
+
+    public void setUserUsername(String userUsername) {
+        this.userUsername = userUsername;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(String userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+
+    public boolean isCollaborator() {
+        return isCollaborator;
+    }
+
+    public void setIsCollaborator(boolean isCollaborator) {
+        this.isCollaborator = isCollaborator;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+}
