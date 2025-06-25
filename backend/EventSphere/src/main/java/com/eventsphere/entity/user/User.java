82c9861d7114569b12a65cb7cf16334730d1e523
Hyorannunes <hyoran2000@gmail.com>
2025-06-24 22:30:33 -0300

    backend: fix: user.java

--- backend/EventSphere/src/main/java/com/eventsphere/entity/user/User.java
+++ backend/EventSphere/src/main/java/com/eventsphere/entity/user/User.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 
 @Entity
+@Table(name = "app_user")
 public class User {
 
     @Id
@@ -17,10 +18,8 @@ public class User {
     private String username;
 
     @Column(nullable = false, unique = false)
-    private String name;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    private String name;    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "app_user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "role", unique = false)
     private Set<String> roles = new HashSet<>();
 
@@ -28,12 +27,14 @@ public class User {
     private String email;
 
     @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private LocalDateTime registerDate;
-
-    @Column(nullable = false, unique = false)
+    private LocalDateTime registerDate;    @Column(nullable = false, unique = false)
     private String password;
 
+    @Column(columnDefinition = "LONGTEXT")
     private String photo;
+
+    @Column(nullable = false)
+    private boolean isBlocked = false;
 
     public User(String username, String name, Set<String> roles, String email, LocalDateTime registerDate, String photo) {
         this.username = username;
@@ -117,5 +118,12 @@ public class User {
 
     public void setPhoto(String photo) {
         this.photo = photo;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
     }
 }
