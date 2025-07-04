032b9a690e538b30df6d4be13680f0d3d1f8bba2
Hyoran <hyoran2000@gmail.com>
2025-07-04 19:19:54 -0300

    feat: footer implementacao de estilizacao

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { IoLogoInstagram, IoLogoLinkedin } from 'react-icons/io5';
 import '../styles/Footer.css';
+import { Link } from './Link';
 import logoFooter from '../images/logo-footer.png';
 
 const Footer = () => (
   <footer className="main-footer">
     <div className="main-footer-left">
-      <span className="main-footer-menu">INICIO<br/>EVENTOS</span>
+      <span className="main-footer-menu"><Link to="/main" className='footer-link'>INICIO</Link><Link to="/eventos" className='footer-link'>EVENTOS</Link></span>
     </div>
     <div className="main-footer-center">
       <img src={logoFooter} alt="EventSphere" className="main-footer-logo" />
